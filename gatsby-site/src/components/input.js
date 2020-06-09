599951da572bfcf8e105460903085c20b5b0b070
Michael Wlodawsky <michaelwlodawsky@gmail.com>
2020-06-09 16:20:45 -0500

    added all inputs to be sent to API request and made successful API request to AWS. Going to need to determine to only allow cors from my website domain instead of everywhere else

--- gatsby-site/src/components/input.js
+++ gatsby-site/src/components/input.js
@@ -1,13 +1,24 @@
 import React from "react"
 
 export default function Input(props) {
+    // props: label, inputName, buttonName, value
     return (
-        <div style={{
-            maxWidth: '500px',
-            //display: 'inline-block'
-        }}>
-            <label for={props.id}>{props.label}</label>
-            <input id={props.id}></input>
+        <div>
+              <label>
+                Email Recepient(s):
+                <input 
+                  type="text" 
+                  name="currentTo"
+                  value={this.state.currentTo}
+                  onChange={this.onSetupInput}
+                  ></input>
+                <button
+                  type="reset"
+                  name="to"
+                  value={this.state.currentTo}
+                  onClick={this.onAddInput}>+</button>
+                {this.state.currentTo}
+              </label>
         </div>
     )
 }
--- gatsby-site/src/pages/index.js
+++ gatsby-site/src/pages/index.js
@@ -1,80 +1,202 @@
 import React from "react"
-import Input from "../components/input"
-import { Link } from "gatsby"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+const TO = "to"
+const CC = "cc"
+const BCC= "bcc"
+const SUBJECT = "subject"
+const BODY = "body"
+const EMAIL = "email"
+const API_URL = "https://dutuq6mkaf.execute-api.us-east-2.amazonaws.com/PROD"
 
-const IndexPage = () => (
-  <div style={{
-    maxWidth: '750px',
-    margin: `auto`
-  }}>
-    <h1 style={{
-      textAlign: 'center'
-    }}>
-      Email4Change
-    </h1>
-    <p style={{
-      textAlign: 'center',
-      maxWidth: 'auto'
-    }}>
-      What's the best way to get your elected officials to change something? Tell them yourself.
-    </p>
-    <h2>
-      Construct an email:
-    </h2>
-    <form>
-      <div>
-        <label>
-          Email Recepient(s):
-          <input type="text" name="to"></input>
-          <button>+</button>
-        </label>
-      </div>
-      <div>
-        <label>
-          CC Recepient(s):
-          <input type="text" name="cc"></input>
-          <button>+</button>
-        </label>
-      </div>
-      <div>
-        <label>
-          BCC Recepient(s):
-          <input type="text" name="bcc"></input>
-          <button>+</button>
-        </label>
-      </div>
-      <div>
-        <label>
-          Email Recepient(s):
-          <input type="text" name="recepients"></input>
-          <button>+</button>
-        </label>
-      </div>
-      <div>
-        <label>
-          Subject:
-          <input type="text" name="subject"></input>
-        </label>
-      </div>
-      <div>
-        <label>
-          Body:
-          <input type="text" name="body"></input>
-        </label>
-      </div>
-      <div>
-        <label>
-          Your Email (to send the link for safe keeping):
-          <input type="text" name="email"></input>
-        </label>
-      </div>
-      <button type="submit">Get your link!</button>
-    </form>
-  </div>
-)
+export default class IndexPage extends React.Component {
 
-export default IndexPage
+
+  state = {
+    currentTo: "",
+    currentCC: "",
+    currentBCC: "",
+    to: [],
+    cc: [],
+    bcc: [],
+    subject: "",
+    body: "",
+    email: ""
+ }
+
+ generateLink = event => {
+   const data = {
+     "to": this.state.to.toString(),
+     "cc": this.state.cc.toString(),
+     "bcc": this.state.bcc.toString(),
+     "subject": this.state.subject,
+     "body": this.state.body,
+     "email": this.state.email
+   }
+   try {
+    fetch(API_URL, {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8'
+      },
+      mode: 'cors'
+    }).then(
+      (result) => {
+        console.log(result)
+        console.log(result.body.url)
+      }
+    )
+   } catch (err) {
+     alert(`Error Generating Link from Server: Code ${err.code}`)
+   }
+
+ }
+
+ onSetupInput = event => {
+   const target = event.target
+   const value = target.value
+   const inputName = target.name
+
+   this.setState({
+     [inputName]: value
+   })
+ }
+
+ onAddInput = event => {
+   const target = event.target
+   const value = target.value
+   const inputName = target.name
+   var newValue = []
+
+   // TODO: Check validity of email?
+
+   switch (inputName) {
+      case TO:
+        newValue = this.state.to.concat(value)
+        break;
+      case CC:
+        newValue = this.state.cc.concat(value)
+        break;
+      case BCC:
+        newValue = this.state.bcc.concat(value)
+        break;
+      default:
+        break;
+   }
+
+
+   this.setState({
+     [inputName]: newValue
+   })
+ }
+
+  render() {
+    return (
+      <div style={{
+          maxWidth: '750px',
+          margin: `auto`
+        }}>
+          <h1 style={{
+            textAlign: 'center'
+          }}>
+            Email4Change
+          </h1>
+          <p style={{
+            textAlign: 'center',
+            maxWidth: 'auto'
+          }}>
+            What's the best way to get your elected officials to change something? Tell them yourself.
+          </p>
+          <h2>
+            Construct an email:
+          </h2>
+          <form>
+            <div>
+              <label>
+                Email Recepient(s):
+                <input 
+                  type="text" 
+                  name="currentTo"
+                  value={this.state.currentTo}
+                  onChange={this.onSetupInput}
+                  ></input>
+                <button
+                  type="reset"
+                  name={TO}
+                  value={this.state.currentTo}
+                  onClick={this.onAddInput}>+</button>
+              </label>
+            </div>
+            {this.state.to}
+            <div>
+              <label>
+                CC Recepient(s):
+                <input 
+                  type="text" 
+                  name="currentCC"
+                  value={this.state.currentCC}
+                  onChange={this.onSetupInput}></input>
+                <button
+                  type="reset"
+                  name={CC}
+                  value={this.state.currentCC}
+                  onClick={this.onAddInput}>+</button>
+              </label>
+            </div>
+            {this.state.cc}
+            <div>
+              <label>
+                BCC Recepient(s):
+                <input 
+                  type="text" 
+                  name="currentBCC"
+                  value={this.state.currentBCC}
+                  onChange={this.onSetupInput}></input>
+                <button
+                  type="reset"
+                  name={BCC}
+                  value={this.state.currentBCC}
+                  onClick={this.onAddInput}>+</button>
+              </label>
+            </div>
+            {this.state.bcc}
+            <div>
+              <label>
+                Subject:
+                <input 
+                  type="text" 
+                  name={SUBJECT}
+                  value={this.state.subject}
+                  onChange={this.onSetupInput}></input>
+              </label>
+            </div>
+            <div>
+              <label>
+                Body:
+                <input 
+                  type="text" 
+                  name={BODY}
+                  value={this.state.body}
+                  onChange={this.onSetupInput}></input>
+              </label>
+            </div>
+            <div>
+              <label>
+                Your Email (to send the link for safe keeping):
+                <input 
+                  type="text" 
+                  name={EMAIL}
+                  value={this.state.email}
+                  onChange={this.onSetupInput}></input>
+              </label>
+            </div>
+            <button 
+              type="button"
+              onClick={this.generateLink}>Get your link!</button>
+          </form>
+        </div>
+    )
+  }
+  
+}
+
